10d10a50763dded63ff62681ee220b3864c85d4d
ts-miller <tylerstephen11@gmail.com>
2020-11-18 21:39:54 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(array, event) {
+    for (let i = 0; i < array.length; i++) {
+        array.splice(i, 1, `Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+    }
+
+    return array;
+}
+
+function countDown(num) {
+    for (let i = num; i >= 0; i--) {
+        console.log(i);
+    }
+}
